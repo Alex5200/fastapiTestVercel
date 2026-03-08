1feda79147cff9717fcf7de1e1c1db46be9986e7
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2026-03-08 22:30:45 +0300

    add exceprion

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# api/index.py
 from fastapi import FastAPI, Depends, HTTPException, status, UploadFile, File, Request
 from fastapi.security import APIKeyHeader
 from fastapi.responses import StreamingResponse, JSONResponse
@@ -7,11 +6,10 @@ from typing import Annotated
 import sys
 import os
 
-# Добавляем parent директорию в path для импортов
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from config import settings
-from s3_client import s3_client
+from services import s3_client
 from middleware import LoggingMiddleware, SecurityHeadersMiddleware
 from exceptions import global_exception_handler, validation_exception_handler
 from fastapi.exceptions import RequestValidationError
@@ -20,18 +18,12 @@ import uuid
 import logging
 from datetime import datetime
 
-# Настройка логирования
-logging.basicConfig(
-    format="%(message)s",
-    level=settings.log_level,
-    stream=sys.stdout
-)
+logging.basicConfig(format="%(message)s", level=settings.log_level, stream=sys.stdout)
 
 logger = logging.getLogger("api")
 
 app = FastAPI(title="PDF Storage API", version="1.0.0")
 
-# Middleware
 app.add_middleware(LoggingMiddleware)
 app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(
@@ -42,25 +34,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Обработчики ошибок
+
 app.add_exception_handler(Exception, global_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 
-async def get_api_key(request: Request, api_key: Annotated[str | None, Depends(api_key_header)]) -> str:
+async def get_api_key(
+    request: Request, api_key: Annotated[str | None, Depends(api_key_header)]
+) -> str:
     if not api_key:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="API Key header missing",
-            headers={"WWW-Authenticate": "ApiKey"}
+            headers={"WWW-Authenticate": "ApiKey"},
         )
     if not settings.is_valid_key(api_key):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid API Key",
-            headers={"WWW-Authenticate": "ApiKey"}
+            headers={"WWW-Authenticate": "ApiKey"},
         )
     return api_key
 
@@ -71,19 +65,26 @@ async def health_check(request: Request):
 
 
 @app.get("/api/v1/files/all")
-async def getAllfiles(
-        request: Request,
-        api_key:Annotated[str, Depends(get_api_key)] = None
+async def getAllFiles(
+    request: Request, api_key: Annotated[str, Depends(get_api_key)] = None
 ):
-    data = await s3_client.getAll_files()
-    print(data)
-    return data
+    try:
+        data = await s3_client.getAll_files()
+        print(data)
+        return data
+    except Exception as e:
+        logger(f"Error getAllFiles {e}")
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Server error"
+        )
+
 
 @app.post("/api/v1/files/upload")
 async def upload_pdf(
-        request: Request,
-        file: UploadFile = File(...),
-        api_key: Annotated[str, Depends(get_api_key)] = None
+    request: Request,
+    file: UploadFile = File(...),
+    api_key: Annotated[str, Depends(get_api_key)] = None,
 ):
     if file.content_type not in ["application/pdf"]:
         raise HTTPException(status_code=400, detail="Only PDF files allowed")
@@ -93,12 +94,12 @@ async def upload_pdf(
     if len(content) > settings.max_file_size:
         raise HTTPException(status_code=400, detail="File too large")
 
-    if not content.startswith(b'%PDF'):
+    if not content.startswith(b"%PDF"):
         raise HTTPException(status_code=400, detail="Invalid PDF content")
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     unique_id = uuid.uuid4().hex[:8]
-    filename = f"pdfs/{timestamp}_{unique_id}_{file.filename}"
+    filename = f"pdf/{timestamp}_{unique_id}_{file.filename}"
 
     await s3_client.upload_file(content, filename, file.content_type)
 
@@ -107,23 +108,25 @@ async def upload_pdf(
 
 @app.get("/api/v1/files/{filename:path}")
 async def download_pdf(
-        request: Request,
-        filename: str,
-        api_key: Annotated[str, Depends(get_api_key)] = None
+    request: Request,
+    filename: str,
+    api_key: Annotated[str, Depends(get_api_key)] = None,
 ):
     content = await s3_client.download_file(filename)
     return StreamingResponse(
         io.BytesIO(content),
         media_type="application/pdf",
-        headers={"Content-Disposition": f"attachment; filename={filename.split('/')[-1]}"}
+        headers={
+            "Content-Disposition": f"attachment; filename={filename.split('/')[-1]}"
+        },
     )
 
 
 @app.get("/api/v1/files")
 async def list_files(
-        request: Request,
-        prefix: str = "pdfs/",
-        api_key: Annotated[str, Depends(get_api_key)] = None
+    request: Request,
+    prefix: str = "pdf/",
+    api_key: Annotated[str, Depends(get_api_key)] = None,
 ):
     files = await s3_client.list_files(prefix)
     return {"count": len(files), "files": files}
@@ -131,9 +134,9 @@ async def list_files(
 
 @app.delete("/api/v1/files/{filename:path}")
 async def delete_pdf(
-        request: Request,
-        filename: str,
-        api_key: Annotated[str, Depends(get_api_key)] = None
+    request: Request,
+    filename: str,
+    api_key: Annotated[str, Depends(get_api_key)] = None,
 ):
     await s3_client.delete_file(filename)
     return {"message": "File deleted", "filename": filename}
